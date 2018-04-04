c280c671ff830b8a71704400f16c597e49c77a83
David Stainton <dstainton415@gmail.com>
2018-04-04 04:28:33 +0000

    Add docstrings and test vectors

--- src/lib.rs
+++ src/lib.rs
@@ -12,7 +12,6 @@ use crypto::digest::Digest;
 use crypto::blake2b::Blake2b;
 use crypto::symmetriccipher::SynchronousStreamCipher;
 use crypto::chacha20::ChaCha20;
-//use serialize::hex::ToHex;
 
 pub mod error;
 pub use error::LionessError;
@@ -27,6 +26,19 @@ const CHACHA20_NONCE_SIZE: usize = 12;
 pub const IV_SIZE: usize = CHACHA20_NONCE_SIZE * 4;
 
 
+/// encrypt a block
+///
+/// # Arguments
+///
+/// * `key` - a key
+/// * `iv`  - an IV
+/// * `dst` - a destination mutable byte slice
+/// * `src` - a source reference to a byte slice of data to encrypt
+///
+/// # Errors
+///
+/// * `LionessError::BlockSizeError` - returned if block size is too small
+///
 pub fn encrypt(key: &[u8; RAW_KEY_SIZE], iv: &[u8; IV_SIZE], dst: &mut [u8], src: &[u8]) -> Result<(), LionessError> {
     let mut hr = [0u8; DIGEST_RESULT_SIZE];
     let mut k = [0u8; STREAM_CIPHER_KEY_SIZE];
@@ -57,8 +69,6 @@ pub fn encrypt(key: &[u8; RAW_KEY_SIZE], iv: &[u8; IV_SIZE], dst: &mut [u8], src
     let mut sc = ChaCha20::new(&k, iv1);
     sc.process(&right, &mut tmp_right);
 
-    // XXX println!("encrypt ROUND 1: {}", right.to_hex());
-
     // L = L ^ BLAKE2b(k2 | iv2, R)
     h.input(&tmp_right);
     h.result(&mut hr);
@@ -84,6 +94,19 @@ pub fn encrypt(key: &[u8; RAW_KEY_SIZE], iv: &[u8; IV_SIZE], dst: &mut [u8], src
     Ok(())
 }
 
+/// decrypt a block
+///
+/// # Arguments
+///
+/// * `key` - a key
+/// * `iv`  - an IV
+/// * `dst` - a destination mutable byte slice
+/// * `src` - a source reference to a byte slice of data to decrypt
+///
+/// # Errors
+///
+/// * `LionessError::BlockSizeError` - returned if block size is too small
+///
 pub fn decrypt(key: &[u8; RAW_KEY_SIZE], iv: &[u8; IV_SIZE], dst: &mut [u8], src: &[u8]) -> Result<(), LionessError> {
     let mut hr = [0u8; DIGEST_RESULT_SIZE];
     let mut k = [0u8; STREAM_CIPHER_KEY_SIZE];
@@ -144,12 +167,16 @@ mod tests {
     extern crate rand;
     extern crate rustc_serialize;
     use super::*;
-    //use crypto::blake2b::Blake2b;
-    //use crypto::chacha20::ChaCha20;
     use self::rand::Rng;
     use self::rand::os::OsRng;
-    //use self::rustc_serialize::hex::FromHex;
-    //use serialize::hex::ToHex;
+    use self::rustc_serialize::hex::FromHex;
+
+    struct Test {
+        input: Vec<u8>,
+        output: Vec<u8>,
+        key: Vec<u8>,
+        iv: Vec<u8>,
+    }
 
     #[test]
     fn simple_encrypt_decrypt_test() {
@@ -168,4 +195,31 @@ mod tests {
         decrypt(&key, &iv, &mut dst2, &dst1).unwrap();
         assert!(dst2 == src)
     }
-}
+
+    fn test_cipher(tests: &[Test]) {
+        for t in tests {
+            let mut dst = vec![0u8; t.input.len()];
+            let mut key = [0u8; RAW_KEY_SIZE];
+            key.copy_from_slice(t.key.as_slice());
+            let mut iv = [0u8; IV_SIZE];
+            iv.copy_from_slice(t.iv.as_slice());
+            encrypt(&key, &iv, &mut dst, t.input.as_slice()).unwrap();
+            let want: Vec<u8> = t.output.as_slice().to_owned();
+            assert_eq!(want, dst)
+        }
+    }
+
+    #[test]
+    fn chach20_blake2b_lioness_vectors_test() {
+        let test_vectors = vec![
+            Test {
+                key: "a5d7bd43320df4f560a3ff85b05f22dfc3d4e7405d53802305be474de7bf1c33d29da3ab7af680cc8ffb0a58111434004c807fea8af67ae01486f0a2be89463f365b910000c7cd87f80a0e8df7e61f083fbe9cd537f6fc33e7c97062351aa3599b767c6fa8a8fb60dac72573e169b34b2dc73a3edfb0cdad47657a83ea559140".from_hex().unwrap(),
+                iv: "2ca1a11bff1713d7d6dbaadf1037d53d6a96092fbf74198371e77bf8c77b346b625cc938c449fdb3a11f1851703a1534".from_hex().unwrap(),
+                input: "3e2e5cab574e9baeb9fb9a6c9c1d629b3876577d677cde37e957538ff76f6fae97f72aa40d4039ba32c9957fa1728cee12e0ad322f25021cd409c816d3a1861d804adb25966d6409f6b3a73ecf6b9f85dbe5411697e98b5d34eef2e9c957f54296988fa4374284dc17c85f27e50b73a333bd55f5220d89e9b513395ff8962d2871cdda248e5d8ff019be8c4ef794f9361fe7623385ff3f17088f8edab0b10b1e298d6da11b743b09dc6593821fa780cd0ac9b187dfc33fbf5f9f566efd23a1c7f602a0e2d16a9bb2973a184640cabbe2726ec13ea9eefdf563cff7cc3827ad6a0cf0040db7180909c5f940a85333880ad957b651d210c86568ce8dc12687ec87b60c4aec2e1a37cc12904f568e14d30e67de4aaf6b70678d3dd609434f2aae6d762c2f04e00a32fe06f1ec2860c7b7abc780137330157a381bc6655d4bf17ed1f4031e052105efe74bb6c1d5215b225dd7388841d9f72df9ce5ca64eba668ae4ac17d5b3c8a7d216cd58c242f4c8ffdb02961fa3880fb616fd7939af01c100457dffc010072c2bf00610257a31ffb9bbf569e1a6604691c5a727ff133fd4fbc0067a012a9bc34f6bd2b6544c126853ada937cdf4b426a603ec4a7c13e06b48752c4fce5d632ca0b4ece880a870976b009956bbfaf4f29b4b22e763c94200db5906d0ab67c221e219e06dd8bffe617112f5f5703a".from_hex().unwrap(),
+                output: "484716632eda1a807a7cc84b4b7824ae968c687cdc2bbe982012a718d924db4b9b33e78374990e2a2cd7f8d46fbedad701960e6db1c7198ae261f3fc6ad23d845df73b2d0e8a8f5f6305a0ea0867d7d16b3a0fff662536090ff5580b3020351c7c004c3f5ca126a03fa46646c460fd50d52f7748c45882bd995a6bc066930df1ec23e247e5cc58a2cfe4224224a784f6e408ec47cffe4a09473526e679e6527ff6144fc7b0ea9559ad8ce474b71b73c3585fb53327dc698e87d9755b452bdb9f8c19791de68101edd492c9a8b10227aae8d39214bb222cae95cd352e06ab67e753d56b5f07c958aef105423e27a4eac8a7e956e90f731d255a5ac3fd41fcef167ffc2f080d42a9bb008a7f32a16b7a787b44bb683729ff8dc2244755d7b124f80c6b9dc9a98fb3ae238ad1a25960beb952aa27a73e85bc0c0ab421b2f4ae6e2829db6103e9f3a8399a1e27f0fcebdc30cd76560fce1b88447ac18a362a3ff3a3bf4b8361fcac8eede3eeeb30472c3cd4d29ebc8d194143d9554362da27387fde22248c7e2d91010a816a368bcc221f5adf17dc004eaedf997a08bbab85cca1cfa42e132dd411a6fe38efe132a0ef5b1ca9ce94d48b2a0b52b10a90101f8dd70e94a887cf2de58af36f759464fa7b771b7a8c803116d1091329824f03829d0a21c5b5a27f5a1d9cb48d3dace7eead9331fc89abaf".from_hex().unwrap(),
+            }];
+
+        test_cipher(&test_vectors[..]);
+    }
+
+} // tests
